c649cea0b2c8edc08040bc7d2d9e84fe5f2803a6
Eugenio <lugezz@gmail.com>
2022-09-22 16:17:16 -0300

    Beautifulsoup

--- py4e/Código/urllinks.py
+++ py4e/Código/urllinks.py
@@ -2,20 +2,26 @@
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
 
-import urllib.request, urllib.parse, urllib.error
+from urllib import request
 from bs4 import BeautifulSoup
 import ssl
+import collections
+
+
+collections.Callable = collections.abc.Callable
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# http://www.dr-chuck.com/page2.htm
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
+html = request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
 tags = soup('a')
+# print(tags)
 for tag in tags:
     print(tag.get('href', None))
