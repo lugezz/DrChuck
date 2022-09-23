3d416dd2d70a2331441559d184d38cf8603bbd6b
Eugenio <lugezz@gmail.com>
2022-09-23 09:02:08 -0300

    Cleanup

--- py4e/Código/geodata/geodump.py
+++ py4e/Código/geodata/geodump.py
@@ -9,26 +9,33 @@ cur.execute('SELECT * FROM Locations')
 fhand = codecs.open('where.js', 'w', "utf-8")
 fhand.write("myData = [\n")
 count = 0
-for row in cur :
+for row in cur:
     data = str(row[1].decode())
-    try: js = json.loads(str(data))
-    except: continue
+    try:
+        js = json.loads(str(data))
+    except Exception:
+        continue
 
-    if not('status' in js and js['status'] == 'OK') : continue
+    if not ('status' in js and js['status'] == 'OK'):
+        continue
 
     lat = js["results"][0]["geometry"]["location"]["lat"]
     lng = js["results"][0]["geometry"]["location"]["lng"]
-    if lat == 0 or lng == 0 : continue
+    if lat == 0 or lng == 0:
+        continue
     where = js['results'][0]['formatted_address']
     where = where.replace("'", "")
-    try :
+
+    try:
         print(where, lat, lng)
 
         count = count + 1
-        if count > 1 : fhand.write(",\n")
+        if count > 1:
+            fhand.write(",\n")
         output = "["+str(lat)+","+str(lng)+", '"+where+"']"
         fhand.write(output)
-    except:
+
+    except Exception:
         continue
 
 fhand.write("\n];\n")
@@ -36,4 +43,3 @@ cur.close()
 fhand.close()
 print(count, "records written to where.js")
 print("Open where.html to view the data in a browser")
-
--- py4e/Código/geodata/geoload.py
+++ py4e/Código/geodata/geoload.py
@@ -1,10 +1,8 @@
-import urllib.request, urllib.parse, urllib.error
-import http
-import sqlite3
 import json
-import time
+import sqlite3
 import ssl
-import sys
+import time
+from urllib import request, parse
 
 api_key = False
 # If you have a Google Places API key, enter it here
@@ -13,7 +11,7 @@ api_key = False
 if api_key is False:
     api_key = 42
     serviceurl = "http://py4e-data.dr-chuck.net/json?"
-else :
+else:
     serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
 
 # Additional detail for urllib
@@ -33,48 +31,48 @@ ctx.verify_mode = ssl.CERT_NONE
 fh = open("where.data")
 count = 0
 for line in fh:
-    if count > 200 :
+    if count > 200:
         print('Retrieved 200 locations, restart to retrieve more')
         break
 
     address = line.strip()
     print('')
-    cur.execute("SELECT geodata FROM Locations WHERE address= ?",
-        (memoryview(address.encode()), ))
+    cur.execute("SELECT geodata FROM Locations WHERE address= ?", (memoryview(address.encode()), ))
 
     try:
         data = cur.fetchone()[0]
-        print("Found in database ",address)
+        print("Found in database", address)
         continue
-    except:
+    except Exception:
         pass
 
     parms = dict()
     parms["address"] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
+    if not api_key:
+        parms['key'] = api_key
+    url = serviceurl + parse.urlencode(parms)
 
     print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
+    uh = request.urlopen(url, context=ctx)
     data = uh.read().decode()
     print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
     count = count + 1
 
     try:
         js = json.loads(data)
-    except:
+    except Exception:
         print(data)  # We print in case unicode causes an error
         continue
 
-    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
+    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS'):
         print('==== Failure To Retrieve ====')
         print(data)
         break
 
     cur.execute('''INSERT INTO Locations (address, geodata)
-            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode()) ) )
+            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode())))
     conn.commit()
-    if count % 10 == 0 :
+    if count % 10 == 0:
         print('Pausing for a bit...')
         time.sleep(5)
 
